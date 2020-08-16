ad51e428c37de29b018d8520c892b7759b5f8e03
Su-Kyung <slloph720@naver.com>
2020-08-16 23:36:02 +0900

    플레이어 위치에 따른 크기 변환
    기영아 크기 기복이 너무 심하다...

---
+++ NewtroFamily/Assets/Script/Game1/CtrlPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlPlayer : MonoBehaviour
+{
+    private Transform sphereTr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphereTr = GetComponent<Transform>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 제기 Bounce 반사각
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Vector3 reflect = collision.transform.position - sphereTr.position;
+
+            float result = 0.0f;
+
+            if (reflect.x > 0) result = 1.0f;
+            else if (reflect.x < 0) result = -1.0f;
+
+            collision.rigidbody.AddForce(new Vector3(150.0f * result, 50.0f, 0.0f));
+        }
+    }
+}
--- NewtroFamily/Assets/Script/Game1/DragPlayer.cs
+++ NewtroFamily/Assets/Script/Game1/DragPlayer.cs
@@ -12,7 +12,12 @@ public class DragPlayer : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+
+        // 오브젝트 최초 크기 저장
+        float originSize = rectTransform.sizeDelta.x;
+        Debug.Log(originSize);
     }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
@@ -25,20 +30,25 @@ public class DragPlayer : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,
         /*
         // 주어진 범위를 벗어나지 않을 때에만 위치 변경 (x)
         if (rectTransform.anchoredPosition.x >= -700 && rectTransform.anchoredPosition.x <= 700)
-            rectTransform.anchoredPosition += eventData.delta;  // x값만 변경해야함..
+            rectTransform.anchoredPosition += eventData.delta;  // x값만 변경해야함
 
         // 주어진 범위를 벗어나지 않을 때에만 위치 변경 (y)
         if (rectTransform.anchoredPosition.y >= -300 && rectTransform.anchoredPosition.y <= 130)
-            rectTransform.anchoredPosition += eventData.delta;  // y값만 변경해야함..
+            rectTransform.anchoredPosition += eventData.delta;  // y값만 변경해야함
         */
 
         rectTransform.anchoredPosition += eventData.delta;
+        float positionY = rectTransform.anchoredPosition.y;
 
+        Debug.Log("현재 y 좌표: " + rectTransform.anchoredPosition.y);
 
-        // 캐릭터 크기 설정
-        if (eventData.delta.y < 0) rectTransform.sizeDelta *= (1 - eventData.delta.y * 0.0011f);
-        else if (eventData.delta.y > 0) rectTransform.sizeDelta *= (1 - eventData.delta.y * 0.001f);
+        // 캐릭터 크기 변환
+        // 변환 key
+        float sizeKey = rectTransform.anchoredPosition.y;
 
+        //if (eventData.delta.y == 0) rectTransform.sizeDelta = new Vector2(originSize, originSize);
+         if (eventData.delta.y < 0) rectTransform.sizeDelta = new Vector2(100 - sizeKey, 100 - sizeKey);
+        else rectTransform.sizeDelta = new Vector2(100 - sizeKey, 100 - sizeKey);
     }
 
     public void OnEndDrag(PointerEventData eventData)
