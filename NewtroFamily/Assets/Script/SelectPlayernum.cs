64edc8f1e8c43d8da9ed8df04d14bc1131f9a8d7
Su-Kyung <slloph720@naver.com>
2020-08-21 04:31:21 +0900

    게임결과 버튼 기능 추가, 이미지디벨롭
    - Select Players 이미지 수정
    - 결과창 티켓 이미지 수정
    - 결과창 다시하기/나가기 버튼 기능 구현

--- NewtroFamily/Assets/Script/SelectPlayernum.cs
+++ NewtroFamily/Assets/Script/SelectPlayernum.cs
@@ -8,7 +8,7 @@ public class SelectPlayernum : MonoBehaviour
     //변수
     //UI
     public GameObject player2,player3,player4,cancle2,cancle3,cancle4;
-    public GameObject p2_nickname, p3_nickname, p4_nickname;
+    //public GameObject p2_nickname, p3_nickname, p4_nickname;
 
     //Resource
     public Sprite dahye, younghee, chulsoo;
@@ -20,9 +20,9 @@ public class SelectPlayernum : MonoBehaviour
     public void Plusplayer2()
     {
         player2.SetActive(true);
-        player2.GetComponent<Image>().sprite = Resources.Load("dahye", typeof(Sprite)) as Sprite;
+        player2.GetComponent<Image>().sprite = Resources.Load("2.0 dahye_new", typeof(Sprite)) as Sprite;
         cancle2.SetActive(true);
-        p2_nickname.SetActive(true);
+        //p2_nickname.SetActive(true);
         player3.SetActive(true);
 
     }
@@ -30,18 +30,18 @@ public class SelectPlayernum : MonoBehaviour
     //플레이어 수 3명
     public void Plusplayer3()
     {
-        player3.GetComponent<Image>().sprite = Resources.Load("younghee", typeof(Sprite)) as Sprite;
+        player3.GetComponent<Image>().sprite = Resources.Load("2.0 younghee_new", typeof(Sprite)) as Sprite;
         cancle3.SetActive(true);
-        p3_nickname.SetActive(true);
+        //p3_nickname.SetActive(true);
         player4.SetActive(true);
     }
 
     //플레이어 수 4명
     public void Plusplayer4()
     {
-        player4.GetComponent<Image>().sprite = Resources.Load("chulsoo", typeof(Sprite)) as Sprite;
+        player4.GetComponent<Image>().sprite = Resources.Load("2.0 chulsoo_new", typeof(Sprite)) as Sprite;
         cancle4.SetActive(true);
-        p4_nickname.SetActive(true);
+        //p4_nickname.SetActive(true);
     }
     
     //캐릭터 2 취소 
@@ -50,19 +50,19 @@ public class SelectPlayernum : MonoBehaviour
         player2.GetComponent<Image>().sprite = Resources.Load("plus", typeof(Sprite)) as Sprite;
         cancle2.SetActive(false);
         player3.SetActive(false);
-        p2_nickname.SetActive(false);
+        //p2_nickname.SetActive(false);
     }
     public void Cancle3()
     {
         player3.GetComponent<Image>().sprite = Resources.Load("plus", typeof(Sprite)) as Sprite;
         cancle3.SetActive(false);
         player4.SetActive(false);
-        p3_nickname.SetActive(false);
+        //p3_nickname.SetActive(false);
     }
     public void Cancle4()
     {
         player4.GetComponent<Image>().sprite = Resources.Load("plus", typeof(Sprite)) as Sprite;
         cancle4.SetActive(false);
-        p4_nickname.SetActive(false);
+        //p4_nickname.SetActive(false);
     }
 }
